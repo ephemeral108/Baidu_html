e486475753567d53a9d0acbec7c19e3024e357da
ephemeral108 <60362333+ephemeral108@users.noreply.github.com>
2020-07-02 00:18:17 +0800

    Add files via upload

---
+++ version3_0/baidu_computer.js
@@ -0,0 +1,27 @@
+//according to different screen adjust display method
+var screen = document.getElementsByClassName("content")[0];
+var inframe = document.getElementsByClassName("inputFrame")[0];
+var submitButton = document.getElementById("submitButton");
+var icon = document.getElementById("icon");
+var font = document.getElementsByClassName("font");
+icon.style.width = "200px";
+screen.style.width = "60%";
+inframe.style.height = "38px";
+submitButton.style.height = "40px";
+$("#submitButton").css("fontSize", "15px");
+$(".inputFrame").css("font-size", "15px");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
